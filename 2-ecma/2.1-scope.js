d47aacec10ee6d8b43e843377b30df66ecf3eb81
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-08-07 18:44:42 -0700

    feat: código de Scope (var, let y const)

---
+++ 2-ecma/2.1-scope.js
@@ -0,0 +1,42 @@
+let age = 30;
+
+// console.log(isOfLegalAge);
+let isOfLegalAge = false;
+
+if (age >= 18) {
+  isOfLegalAge = true;
+  console.log('eres mayor de edad');
+  console.log(isOfLegalAge)
+} else {
+  console.log('eres menor de edad');
+  console.log(isOfLegalAge)
+}
+
+console.log(isOfLegalAge)
+
+var firstName = 'Yaxche';
+var firstName = 'Manrique';
+var firstName = 'Fulano';
+
+console.log(firstName);
+
+let i = 1;
+i = 2;
+i = 3;
+
+// const j = 0;
+// j = 1;
+
+// const k;
+
+let arr1= [1, 2, 3, 4]
+arr1.push(5)
+
+console.log(arr1)
+
+const arr2 = [9, 8, 7, 6]
+arr2.push(5)
+
+console.log(arr2)
+
+// arr2 = ['a', 'b', 'c', 'd']
